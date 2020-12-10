e0a8c2bf9d51c22bf3bb3a63fdd1f08aefb3b25e
Amy Hou <amy.hou.au@gmail.com>
2020-12-10 17:02:56 +1100

    form id in html and js file

--- js/index.js
+++ js/index.js
@@ -8,35 +8,52 @@ function getConfirmation() {
     return false;
   }
 }
+
 // Select the New Task Form
-// Select the New Task Form
-const newTaskForm = document.querySelector("#newTaskForm");
+ const newTaskForm = document.querySelector("#newTaskForm");
 
 // Add an 'onsubmit' event listener
-newTaskForm.addEventListener("submit", (event) => {
-  // Prevent default action
-  event.preventDefault();
+newTaskForm.addEventListener('submit', (event) => {
+    // Prevent default action
+    event.preventDefault();
 
-  // Select the inputs
-  const newTaskNameInput = document.querySelector("#newTaskNameInput");
-  const newTaskDescription = document.querySelector("#newTaskDescription");
-  const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
-  const newTaskDueDate = document.querySelector("#newTaskDueDate");
-  const errorMessage = document.querySelector("#alertMessage");
-
-  /*
+    // Select the inputs
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const newTaskDescription = document.querySelector('#newTaskDescription');
+    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+    const newTaskDueDate = document.querySelector('#newTaskDueDate');
+    const errorMessage = document.querySelector('#alertMessage');
+    
+    
+    /*
         Validation code here
     */
 
-  // Get the values of the inputs
-  const name = newTaskNameInput.value;
-  const description = newTaskDescription.value;
-  const assignedTo = newTaskAssignedTo.value;
-  const dueDate = newTaskDueDate.value;
-  if (!validFormFieldInput(name)) {
-    errorMessage.innerHTML = "Invalid name input";
-    errorMessage.style.display = "block";
-  } else {
-    errorMessage.style.display = "none";
+    // Get the values of the inputs
+    const name = newTaskNameInput.value;
+    alert(name);
+    const description = newTaskDescription.value;
+    const assignedTo = newTaskAssignedTo.value;
+    const dueDate = newTaskDueDate.value;
+    if(!validFormFieldInput(name)){
+        errorMessage.innerHTML = "Invalid name input";
+        errorMessage.style.display = "block"
+    }else{
+        errorMessage.style.display = "none"
   }
+      if (!isValidDate(dueDate)) {
+        errorMessage.innerHTML = "Invalid date input";
+        errorMessage.style.display = "block";
+      } else {
+        errorMessage.style.display = "none";
+      }
+
 });
+
+function validFormFieldInput(data){
+    return data !== null && data !== '';
+}
+// date validation
+function isValidDate(d) {
+  return !isNaN(new Date(d).getTime());
+}
