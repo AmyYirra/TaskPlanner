e2290c41ecf443d53813f5165ee4d283575853bb
Cecilia <cecilia.vinita@gmail.com>
2020-12-18 12:42:30 +1100

    Removed unnecessary code

--- js/taskManager.js
+++ js/taskManager.js
@@ -3,9 +3,39 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
   const value = "idDone" + id;
   const btnst = "btn_Status" + id;
   // alert(value);
+  if (status=="Done"){
+return `<div class="card p-0 m-0  col-lg-3 col-md-6 mb-4">
+                    <!-- <img src="..." class="card-img-top" alt="..." /> -->
+                    <div class="card-body p-0 m-0 border border-dark ">
+                      <p class="card-title bg-green cardheader">
+                       <span class="p-1 m-1"> Task:  ${name}</span>
+                    </p>
+                      <p class="card-text p-1 m-1 text-left">
+                        <span>Description :</span>
+                      </p>
+                      <p
+                        class="card-text spantextarea p-1 m-1 text-left"
+                        id="viewComment"
+                      >
+                        <span class="spantextarea p-0 m-0"
+                          >${description}</span
+                        >
+                      </p>
 
-  
-  return `<div class="card p-0 m-0  col-lg-3 col-md-6 mb-4">
+                      <p class="card-text p-1 m-1 text-left">
+                        <span>Assigned to :${assignedTo}</span>
+                      </p>
+                      <p class="card-text p-1 m-1 text-left">
+                        <span id=${value} class= " cardheader">Status:${status}</span>
+                      </p>
+                      <p class="card-text p-1 m-1 text-left">
+                        <span>Due Date:${dueDate} </span>
+                      </p>
+                       <p hidden = "hidden"> <span ><input type="textbox" id="txt"  value= ${id} ></span></p>
+                       </div> </div>`;
+  }
+  else{
+    return `<div class="card p-0 m-0  col-lg-3 col-md-6 mb-4">
                     <!-- <img src="..." class="card-img-top" alt="..." /> -->
                     <div class="card-body p-0 m-0 border border-dark ">
                       <p class="card-title bg-green cardheader">
@@ -32,8 +62,8 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
                       <p class="card-text p-1 m-1 text-left">
                         <span>Due Date:${dueDate} </span>
                       </p>
-                      <div>
-                        <p class="card-text p-1 m-1 text-left">
+                       
+                       <div> <p class="card-text p-1 m-1 text-left">
                           <span>
                             
                             <button id=${btnst}
@@ -45,7 +75,7 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
                             >
                             Mark as done
                             </button> </span>
-                    <span hidden><input type="textbox" id="txt"  value= ${id} ></span>
+                   <span hidden = "hidden" ><input type="textbox" id="txt"  value= ${id} ></span>
                           <!--<span>
                             <button
                               class="card-link btn btn-success"
@@ -54,18 +84,19 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
                               Delete
                             </button></span
                           >-->
-                        </p>
+                       
                       </div>
                     </div>
-                  </div>
+                  </div>`;
 
-`;
+  }
+ 
 };
 // task manager class
 class TaskManager {
-  constructor(currentId) {
+  constructor(currentId=0) {
     this.tasks = [];
-    this.currentId = currentId++;
+    this.currentId = currentId +1;
     //alert(this.currentId);
   }
 
@@ -90,18 +121,10 @@ class TaskManager {
 
     for (let i = 0; i < this.tasks.length; i++) {
       const task = this.tasks[i];
-     
-      //  Create a date variable, storing a new Date(), passing in the current task's dueDate to the Date constructor.
-      //const date = new Date();
-      // Create a formattedDate variable, storing a readable string representing the date, using methods of the date we just created.
-      //show us only today date-commented by cecilia on 17/12/2020
-      // const formattedDate =
-      //date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-
-      // Create a taskHtml variable to store the HTML of the current task, by calling the createTaskHtml function and using the properties of the current task, as well as the new formattedDate variable for the parameters.
-     
+    // Create a taskHtml variable to store the HTML of the current task, by calling the createTaskHtml function and using the properties of the current task, as well as the new formattedDate variable for the parameters.
+    
       const taskHtml = createTaskHtml(
-        i,
+        task.id,
         task.name,
         task.description,
         task.assignedTo,
@@ -117,11 +140,24 @@ class TaskManager {
     const tasksList = document.querySelector("#addTask");
     tasksList.innerHTML = tasksHtml;
   } //end of render
+  getTaskById(id){
+    let foundTask;
+    for (let k = 0; k < this.tasks.length ; k++) {
+      const value = this.tasks[k];
+      if (this.tasks[k].id === id) {
+      //  alert(id);
+       foundTask = value;
+      }
+    }
+    return foundTask;
+
+   
+  }
 }
 // end of class
 // //  instances of TaskManager
 const newTaskList = new TaskManager(0);
-let id = 0;
+
 
 //newTaskList.addTask("cooking", "prepare recipe", "Tom", "17-12-2020", "toDo");
 //   newTaskList.addTask("pay bills", "electricity/gas/water", "Sam", "30-12-2020", "toDo");
