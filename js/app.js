a99fcd9e2704dd0121f1a62e239099c78c40cedb
Amy Hou <amy.hou.au@gmail.com>
2021-01-14 12:19:45 +1100

    render the webpage

--- js/app.js
+++ js/app.js
@@ -28,48 +28,20 @@ const getDataForCity = (city) =>
       weatherContainer.innerHTML = cardHtml;
     })
     .catch((error) => {
-      weatherContainer.innerHTML = `<em>Server returned error: "${error.message}".</em>`;
+      //
+      weatherContainer.innerHTML = "City not found";
+      // weatherContainer.innerHTML = `<em>Server returned error: "${error.message}".</em>`;
     });
 
 // createCardHtml function used to render the weather info
-const createCardHtml = (
-  name,
-  emoji,
-  temp,
-  feelsLike,
-  description
-) => `<div class="card" style="width: 18rem;">
-  <div class="card-body">
-  <h5 class="card-title">${name}</h5>
-    <p class="card-text">
-      ${emoji}${description}
-    </p>
+const createCardHtml = (name, emoji, temp, feelsLike, description) => `
+  
+   <p> ${name} ${description} ${temp}&#8451 ${emoji}, feels like ${feelsLike}&#8451</p>
+          
     
-    <h6 class="card-subtitle mb-2 text-muted">
-      ${temp}&#8451, feels like ${feelsLike}&#8451
-    </h6>
-  </div>
-</div>
+  
 `;
 
-  // <div class="card">
-  //   <div class="row no-gutters align-items-center">
-  //     <div class="col-2 h2 pl-1 pt-1 text-center">${emoji}</div>
-  //     <div class="col-10">
-  //       <div class="card-body">
-  //         <div class="row card-title justify-content-between align-items-center mr-3 mb-1">
-  //           <h4>${name}</h4>
-  //           <h6>
-  //             ${temp}c, feels like ${feelsLike}c
-  //           </h6>
-  //         </div>
-  //         <div class="row">
-  //           <h5 class="card-subtitle text-muted">${description}</h5>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
 // emojis object used to find the right emoji from the icon code sent from the api
 const emojis = {
   "01d": "☀️",
@@ -93,14 +65,15 @@ const emojis = {
 };
 
 // selecting all the things needed
-
+const goButton = document.querySelector("#go-button");
 const cityInput = document.querySelector("#city-input");
 const weatherContainer = document.querySelector("#weather-container");
 
 // event listener for a click event on the "Go!" button
-function goButton(){
+goButton.addEventListener("click", (e) => {
   // get the city from the input field
   const city = cityInput.value;
+  // alert();
   // get the weather data for the city
   getDataForCity(city);
-};
+});
